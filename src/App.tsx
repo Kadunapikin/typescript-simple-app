8649c22d799c9481446d7ef3b355a745c39375f4
kadunapikin <ibakoshi27@gmail.com>
2023-10-31 18:13:29 +0100

    add a function

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,19 @@ function App() {
   const age: number = 32;
   const isMarried: boolean = false;
 
-
+//Adding a function
+const getName = (name: string) => {
+  if (name === 'Isaac') {
+    return 32;
+  }
+}
 
   return (
     <div className="App">
       <h1>{name}</h1>
       <h2>is {age} years old</h2>
       {isMarried}
+      <button>Click me</button>
 
     </div>
   );
