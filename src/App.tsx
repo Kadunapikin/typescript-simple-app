64adbfd7e9153b86f1e1f9b884fc77b01e22068f
kadunapikin <ibakoshi27@gmail.com>
2023-10-31 18:50:09 +0100

    create Person component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, {FC} from 'react'
 import './App.css';
 
-function App() {
-  const name: string = 'Isaac';
-  const age: number = 32;
-  const isMarried: boolean = false;
 
-//Adding a function
-const getName = (name: string) => {
-  if (name === 'Isaac') {
-    return 32;
-  }
-}
-
+const App: FC = () => {
   return (
-    <div className="App">
-      <h1>{name}</h1>
-      <h2>is {age} years old</h2>
-      {isMarried}
-      <button>Click me</button>
+    <div>
 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+//Introduction to typescript
+
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   const name: string = 'Isaac';
+//   const age: number = 32;
+//   const isMarried: boolean = false;
+
+// //Adding a function
+// const getName = (name: string) => {
+//   if (name === 'Isaac') {
+//     return 32;
+//   }
+// }
+
+//   return (
+//     <div className="App">
+//       <h1>{name}</h1>
+//       <h2>is {age} years old</h2>
+//       {isMarried}
+//       <button>Click me</button>
+
+//     </div>
+//   );
+// }
+
+// export default App;
