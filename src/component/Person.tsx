d18051d3dd053b1dd672c260a1bbadf4fe541cf7
kadunapikin <ibakoshi27@gmail.com>
2023-10-31 19:08:15 +0100

    render Person component using destructuring method

--- src/component/Person.tsx
+++ src/component/Person.tsx
@@ -5,13 +5,27 @@ interface Props {
     age: number;
     email: string;
 }
+//second option
+export const Person = ({ name, age, email}: Props) => {
+    return (
+      <div>
+        <h1>{name}</h1>
+        <h1>{email}</h1>
+        <h1>{age}</h1>
+      </div>
+    );
+  }
+  
 
-export const Person = (props: Props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.email}</h1>
-      <h1>{props.age}</h1>
-    </div>
-  );
-}
+
+//First method
+
+// export const Person = (props: Props) => {
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h1>{props.email}</h1>
+//       <h1>{props.age}</h1>
+//     </div>
+//   );
+// }
