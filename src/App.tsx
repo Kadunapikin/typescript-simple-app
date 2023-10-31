5c9b057b4bc657b9ba976b90ef0c1ea563825bc2
kadunapikin <ibakoshi27@gmail.com>
2023-10-31 14:42:16 +0100

    create a boolean variable and render all variables

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import './App.css';
 function App() {
   const name: string = 'Isaac';
   const age: number = 32;
-  
+  const isMarried: boolean = false;
+
 
 
   return (
     <div className="App">
-      {name}
+      <h1>{name}</h1>
+      <h2>is {age} years old</h2>
+      {isMarried}
+
     </div>
   );
 }
